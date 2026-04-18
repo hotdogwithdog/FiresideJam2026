e4be40b04d8998f9d31869e159fe19f0cda9dcc0
hotdogwithdog <hotdogwithdog@gmail.com>
2026-04-18 16:36:53 +0200

    Added a prototype to the slime

---
+++ Assets/Src/Inputs/InputReader.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Inputs.Generated;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Inputs
+{
+    public class InputReader : Utilities.Singleton<InputReader>, Actions.IPlayerActions
+    {
+        public Actions actions;
+        
+        #region PlayerEvents
+
+        public Action<Vector2> onMove = delegate { };
+        public Action onJump = delegate { };
+
+        #endregion
+        
+        #region ActivatesAndDisables
+        private void Awake()
+        {
+            base.Awake();
+            actions = new Actions();
+            actions.Disable();
+        }
+
+        private void OnEnable()
+        {
+            if (actions == null)
+            {
+                Debug.LogError("No actions found");
+                return;
+            }
+            
+            actions.Player.SetCallbacks(this);
+            //actions.UI.SetCallbacks(this);
+            
+            actions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            actions.Player.SetCallbacks(null);
+            actions.UI.SetCallbacks(null);
+            actions.Disable();
+        }
+
+
+        public void EnablePlayerActions()
+        {
+            actions.Player.Enable();
+            actions.UI.Disable();
+        }
+
+        public void EnableUIActions()
+        {
+            actions.UI.Enable();
+            actions.Player.Disable();
+        }
+
+        public void DisableAllActions()
+        {
+            actions.Player.Disable();
+            actions.UI.Disable();
+        }
+        #endregion
+        
+        #region PlayerMapCallbacks
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed || context.phase == InputActionPhase.Canceled) 
+                onMove.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            Debug.Log("Attack");
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            Debug.Log("Interact");
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed) onJump.Invoke();
+        }
+        #endregion
+    }
+}
---
+++ Assets/Src/SoftBody.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class SoftBody : MonoBehaviour
+{
+    [SerializeField] private float _offset = 0.5f;
+    
+    [SerializeField] private SpriteShapeController _spriteShapeController;
+    [SerializeField] private Transform[] _points;
+
+    private void Awake()
+    {
+        UpdateVertices();
+    }
+
+    private void Update()
+    {
+        UpdateVertices();
+    }
+
+    private void UpdateVertices()
+    {
+        for (int i = 0; i < _points.Length; ++i)
+        {
+            Vector2 vertex = _points[i].localPosition;
+
+            Vector2 pointToCenter = -vertex.normalized;
+            
+            float colliderRadius = _points[i].gameObject.GetComponent<CircleCollider2D>().radius;
+            try
+            {
+                _spriteShapeController.spline.SetPosition(i, vertex - pointToCenter * colliderRadius);
+            }
+            catch
+            {
+                Debug.Log("Spline points are too close to each other.. recalculate them");
+                _spriteShapeController.spline.SetPosition(i, vertex - pointToCenter * (colliderRadius + _offset));
+            }
+
+            Vector2 leftTangent = _spriteShapeController.spline.GetLeftTangent(i);
+            
+            Vector2 newRightTangent = Vector2.Perpendicular(pointToCenter) * leftTangent.magnitude;
+            
+            _spriteShapeController.spline.SetLeftTangent(i, -newRightTangent);
+            _spriteShapeController.spline.SetRightTangent(i, newRightTangent);
+        }
+    }
+}
