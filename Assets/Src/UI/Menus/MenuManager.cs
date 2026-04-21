ea824e635d14d26a5fd94bd82b35ce1fc0b2e4a4
Kaizzendev <Kaizzendev@gmail.com>
2026-04-21 18:00:32 +0200

    wip (menus start fsm)

---
+++ Assets/Src/UI/Menus/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UI.Menus.States;
+using UnityEngine;
+
+namespace UI.Menus
+{
+    public class MenuManager: Utilities.Singleton<MenuManager>
+    {
+        private IState _currentState;
+
+
+        private void Start()
+        {
+            SetState(new Main());
+        }
+        
+        public void SetState(IState newState)
+        {
+            _currentState?.Exit();
+            _currentState = newState;
+            _currentState.Enter();
+        }
+
+        public IState GetState()
+        {
+            return _currentState;
+        }
+
+        public void Update()
+        {
+            _currentState?.Update(Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Src/UI/Menus/States/Main.cs
@@ -0,0 +1,20 @@
+﻿namespace UI.Menus.States
+{
+    public class Main: IState
+    {
+        public void Enter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(float deltaTime)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
