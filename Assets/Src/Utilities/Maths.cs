1d7623c57a45b55659b66b7ce74530acf6b8671c
hotdogwithdog <hotdogwithdog@gmail.com>
2026-04-21 15:33:17 +0200

    Feat (Added the AddForce Function to the SoftBody object)

---
+++ Assets/Src/Utilities/Maths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class Maths
+    {
+        /// <summary>
+        /// Rotate a point in 2D by an angle, the rotation direction is counter-clockwise for positive angles
+        /// </summary>
+        /// <param name="angle"> The angle to rotate in radians </param>
+        /// <param name="point"> The point to rotate </param>
+        /// <returns></returns>
+        public static Vector2 Rotate2D(float angle, Vector2 point) 
+        {
+            return new Vector2(MathF.Cos(angle) * point.x - MathF.Sin(angle) * point.y, MathF.Sin(angle) * point.x + MathF.Cos(angle) * point.y);
+        }
+    }
+}
