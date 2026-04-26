baf6e016e114526dbfefefeaf98dcc8ef0ddff17
hotdogwithdog <hotdogwithdog@gmail.com>
2026-04-26 18:18:34 +0200

    Feat (EPIC CREDITS SCENE)

---
+++ Assets/Src/Player/ObjectFollower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class ObjectFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _offsetY; 
+
+        private void Update()
+        {
+            transform.position = new Vector3(_target.position.x, _target.position.y + _offsetY, _target.position.z);
+        }
+    }
+}
